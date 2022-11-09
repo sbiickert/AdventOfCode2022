69bb25a2d28692aaad2af4b1c6a8dfea16b048d3
Simon Biickert <simon@biickert.ca>
2022-11-09 07:34:22 -0700

    Building Perl utils that are non-OO

--- Perl/AOC/SpatialUtil.pm
+++ Perl/AOC/SpatialUtil.pm
@@ -8,13 +8,17 @@ use lib $local_lib;
 package AOC::SpatialUtil;
 use Modern::Perl 2018;
 use Exporter;
+use List::Util qw(min max);
 use feature 'signatures';
 
 our @ISA = qw( Exporter );
 #our @EXPORT_OK = qw(C2D_create C3D_create);
 our @EXPORT = qw(C2D_create C2D_to_str C2D_from_str 
 				 C2D_equals C2D_delta C2D_distance C2D_manhattan
-				 C3D_create C3D_equals);
+				 C3D_create C3D_to_str C3D_from_str 
+				 C3D_equals C3D_delta C3D_distance C3D_manhattan
+				 E2D_create E2D_build E2D_to_str
+				 E2D_min E2D_max E2D_width E2D_height E2D_area E2D_contains);
 
 # -------------------------------------------------------
 # Coord2D
@@ -64,20 +68,97 @@ sub C3D_create($x, $y, $z) {
 	return [$x, $y, $z];
 }
 
+sub C3D_to_str($c2d) {
+	return '[' . $c2d->[0] . ',' . $c2d->[1] . ',' . $c2d->[2] . ']';
+}
+
+sub C3D_from_str($val) {
+	if ($val =~ m/\[(-?\d+),(-?\d+),(-?\d+)\]/) {
+		return C3D_create($1, $2, $3);
+	}
+	return 0;
+}
+
 sub C3D_equals($c1, $c2) {
 	return $c1->[0] == $c2->[0] &&
 			$c1->[1] == $c2->[1] &&
 			$c1->[2] == $c2->[2];
 }
 
+sub C3D_delta($c1, $c2) {
+	return C3D_create($c2->[0] - $c1->[0],
+					  $c2->[1] - $c1->[1],
+					  $c2->[2] - $c1->[2]);
+}
+
+sub C3D_distance($c1, $c2) {
+	my $delta = C3D_delta($c1, $c2);
+	return sqrt($delta->[0]**2 + $delta->[1]**2 + $delta->[2]**2);
+}
+
+sub C3D_manhattan($c1, $c2) {
+	my $delta = C3D_delta($c1, $c2);
+	return abs($delta->[0]) + abs($delta->[1]) + abs($delta->[2]);
+}
+
 
 # -------------------------------------------------------
 # Extent2D
+#
+# Data model: array reference [xmin,ymin,xmax,ymax]
 # -------------------------------------------------------
+sub E2D_create($c_min, $c_max) {
+	return [$c_min->[0], $c_min->[1], $c_max->[0], $c_max->[1]];
+}
 
+sub E2D_build(@c_list) {
+	my @data = (0) x 4;
+	if (scalar(@c_list) > 0) {
+		my $c = $c_list[0];
+		@data = ($c->[0], $c->[1], $c->[0], $c->[1]);
+	}
+	for my $c ( @c_list ) {
+		$data[0] = min($data[0], $c->[0]);
+		$data[1] = min($data[1], $c->[1]);
+		$data[2] = max($data[2], $c->[0]);
+		$data[3] = max($data[3], $c->[1]);
+	}
+	return \@data;
+}
+
+sub E2D_to_str($e2d) {
+	return '{min: [' . $e2d->[0] . ',' . $e2d->[1] . '], max: [' . $e2d->[2] . ',' . $e2d->[3] . ']}';
+}
+
+sub E2D_min($e2d) {
+	return C2D_create($e2d->[0], $e2d->[1]);
+}
+
+sub E2D_max($e2d) {
+	return C2D_create($e2d->[2], $e2d->[3]);
+}
+
+sub E2D_width($e2d) {
+	return $e2d->[2] - $e2d->[0]
+}
+
+sub E2D_height($e2d) {
+	return $e2d->[3] - $e2d->[1]
+}
+
+sub E2D_area($e2d) {
+	return E2D_width($e2d) * E2D_height($e2d);
+}
+
+sub E2D_contains($e2d, $c2d) {
+	return $e2d->[0] <= $c2d->[0] && $c2d->[0] <= $e2d->[2] &&
+			$e2d->[1] <= $c2d->[1] && $c2d->[1] <= $e2d->[3];
+}
 
 # -------------------------------------------------------
 # Grid2D
+#
+# Data model: array reference [data hashref, default, rule]
 # -------------------------------------------------------
 
 
--- Perl/_test_spatialutil.pl
+++ Perl/_test_spatialutil.pl
@@ -14,11 +14,12 @@ use AOC::Util;
 use AOC::SpatialUtil;
 use Data::Dumper;
 
-test_coord2d();
-test_coord3d();
-
+#test_coord2d();
+#test_coord3d();
+test_extent2d();
 
 sub test_coord2d {
+	say "Testing Coord2D";
 	my $c2d = C2D_create(10, 30);
 	say C2D_to_str($c2d);
 	
@@ -39,11 +40,40 @@ sub test_coord2d {
 }
 
 sub test_coord3d {
+	say "Testing Coord3D";
 	my $c3d = C3D_create(10, 30, -5);
-	print Dumper($c3d);
+	say C3D_to_str($c3d);
 	
 	my $other = C3D_create(10,30,-5);
+	say 'other: ' . C3D_to_str($other);
 	print "Other is equal? " . C3D_equals($c3d, $other) . "\n";
-	$other = C3D_create(10,20,0);
+	$other = C3D_create(5,20,15);
+	say 'other: ' . C3D_to_str($other);
 	print "Other is equal? " . C3D_equals($c3d, $other) . "\n";
+	
+	my $delta = C3D_delta($c3d, $other);
+	say "Delta from c3d to other is " . C3D_to_str($delta);
+	say "Distance from c3d to other is " . C3D_distance($c3d, $other);
+	say "Manhattan dist from c3d to other is " . C3D_manhattan($c3d, $other);
+	
+	my $clone = C3D_from_str( C3D_to_str($c3d) );
+	say 'The clone ' . C3D_to_str($clone) . ' is the same as the original.';
+}
+
+sub test_extent2d {
+	say "Testing Extent2D";
+	my $c1 = C2D_create(1,1);
+	my $c2 = C2D_create(2,2);
+	my $c3 = C2D_create(3,3);
+	my $c4 = C2D_create(4,4);
+	my $e1 = E2D_create($c1, $c2);
+	say E2D_to_str($e1);
+	my @c_list = ($c3, $c2, $c1);
+	my $e2 = E2D_build(@c_list);
+	say E2D_to_str($e2);
+	say 'The width of e2 is ' . E2D_width($e2);
+	say 'The height of e2 is ' . E2D_height($e2);
+	say 'The area of e2 is ' . E2D_area($e2);
+	say (E2D_contains($e2, $c2) ? 'c2 is contained by e2' : 'c2 is outside e2');
+	say (E2D_contains($e2, $c4) ? 'c4 is contained by e2' : 'c4 is outside e2');
 }
