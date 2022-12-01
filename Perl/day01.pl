660ea386a76a386b766010f7d7161d9e697d16a5
Simon Biickert <simon@biickert.ca>
2022-12-01 08:20:08 -0700

    Day 1 part 1 complete in Perl

---
+++ Perl/day01.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+BEGIN {
+    use Cwd;
+    our $directory = cwd;
+    our $local_lib = $ENV{"HOME"} . '/perl5/lib/perl5';
+}
+
+use lib $directory;
+use lib $local_lib;
+
+use Modern::Perl 2018;
+use autodie;
+use List::Util qw(max);
+#use Data::Dumper;
+#use Storable 'dclone';
+
+use AOC::Util;
+#use AOC::SpatialUtil;
+
+my $INPUT_PATH = '../input';
+#my $INPUT_FILE = 'day01_test.txt';
+my $INPUT_FILE = 'day01_challenge.txt';
+my @input = read_grouped_input("$INPUT_PATH/$INPUT_FILE");
+
+say "Advent of Code 2022, Day 01: Calorie Counting";
+
+my @elf_calories = solve_part_one(@input);
+#solve_part_two(@input);
+
+exit( 0 );
+
+sub solve_part_one {
+	my @groups = @_;
+	
+	my @all_elf_calories = ();
+	
+	for my $g (@groups) {
+		my $elf_calories = 0;
+		my @group = @{$g};
+		for my $num (@group) {
+			$elf_calories += $num;
+		}
+		push( @all_elf_calories, $elf_calories);
+	}
+	
+	my $max_calories = max(@all_elf_calories);
+	say "Part One: the largest number of calories an elf is carrying is $max_calories.";
+	
+	return @all_elf_calories;
+}
+
+sub solve_part_two {
+	my @input = @_;
+}
