bd776eeee5dfde9eb018ddad12ee255b2a4d7a17
Simon Biickert <simon@biickert.ca>
2022-12-01 08:20:31 -0700

    Putting SpatialUtil together in PHP

--- PHP/AoC/SpatialUtil.php
+++ PHP/AoC/SpatialUtil.php
@@ -9,6 +9,10 @@ class Coord2D {
 		return new Coord2D($matches[1], $matches[2]);
 	}
 	
+	public static function zero(): Coord2D {
+		return new Coord2D(0,0);
+	}
+		
 	function __construct(int $x, int $y) {
 		$this->x = $x;
 		$this->y = $y;
@@ -45,7 +49,77 @@ class Coord2D {
 		return sqrt($delta->x * $delta->x + $delta->y * $delta->y);
 	}
 	
+	public function clone(): Coord2D {
+		return new Coord2D($this->x, $this->y);
+	}
+	
 	public function toString(): string {
 		return "[$this->x,$this->y]";
+	}
+}
+
+class Extent2D {
+	protected Coord2D $min;
+	protected Coord2D $max;
+	
+	static function build(array $coords): Extent2D {
+		assert(count($coords) >= 2, 'Not enough points passed to Extent2D::build');
+		$ext = new Extent2D($coords[0], $coords[1]);
+		for ($i = 2; $i < count($coords); $i++) {
+			$ext->expandToFit($coords[$i]);
+		}
+		return $ext;
+	}
+		
+	function __construct(Coord2D $a, Coord2D $b) {
+		# Not going to trust inputs are min and max
+		$xmin = min($a->getX(), $b->getX());
+		$xmax = max($a->getX(), $b->getX());
+		$ymin = min($a->getY(), $b->getY());
+		$ymax = max($a->getY(), $b->getY());
+		$this->min = new Coord2D($xmin, $ymin);
+		$this->max = new Coord2D($xmax, $ymax);
+	}
+	
+	function expandToFit(Coord2D $c) {
+		
+	}
+	
+	function getMin(): Coord2D {
+		return $this->min->clone();
+	}
+	
+	function getMax(): Coord2D {
+		return $this->max->clone();
+	}
+	
+	function getWidth(): int {
+		return $this->max->x = $this->min->x;
+	}
+	
+	function getHeight(): int {
+		return $this->max->y = $this->min->y;
+	}
+	
+	function getArea(): int {
+		return $this->getWidth() * $this->getHeight();
+	}
+	
+	function getAllCoords(): array {
+		$coords = array();
+		foreach (range($this->min->getX(), $this->max->getX()) as $x) {
+			foreach (range($this->min->getY(), $this->max->getY()) as $y) {
+				array_push( $coords, new Coord2D($x, $y) );
+			}
+		}
+		return $coords;
+	}
+	
+	function equalTo(Extent2D $other): bool {
+		return ($this->min->equalTo($other->min) && $this->max->equalTo($other->max));
+	}
+	
+	function toString(): string {
+		return '{min: [' . $this->min->getX() . ',' . $this->min->getY() . '], max: [' . $this->max->getX() . ',' . $this->max->getY() . ']}';
 	}
 }
