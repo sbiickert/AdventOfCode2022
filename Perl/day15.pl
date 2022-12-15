ba6bdb10baa7c978f1a18101a2a92777026b0fe0
Simon Biickert <simon@biickert.ca>
2022-12-15 09:50:50 -0700

    Day 15 part 2 complete in Perl
    A mix of brute force and 1D extents (ranges)

--- Perl/day15.pl
+++ Perl/day15.pl
@@ -28,51 +28,17 @@ our %beacons;
 
 parse_input();
 
+# get_impossible_ranges(3363767);
+# die;
 solve_part_one();
-#solve_part_two(@input);
+solve_part_two();
 
 exit( 0 );
 
 sub solve_part_one {
 	# Test vs Challenge
 	my $search_row = scalar(keys %sensors) > 15 ? 2000000 : 10;
-	
-	# Identify sensors that are within their md of the search row
-	my @close_sensors = ();
-	for my $key (keys %sensors) {
-		my $sensor = $sensors{$key};
-		my $dist_to_search = abs($sensor->{'pos'}[1] - $search_row);
-		push(@close_sensors, $sensor) if $dist_to_search <= $sensor->{'md'};
-	}
-	
-	my @ranges = ();
-	for my $sensor (@close_sensors) {
-		my $dist_to_search = abs($sensor->{'pos'}[1] - $search_row);
-		my $xmin = $sensor->{'pos'}[0] - $sensor->{'md'} + $dist_to_search;
-		my $xmax = $sensor->{'pos'}[0] + $sensor->{'md'} - $dist_to_search;
-		my $r = E1D_create($xmin, $xmax);
-		#say "sensor at " . C2D_to_str($sensor->{'pos'}) . " with MD of $sensor->{'md'} means no beacons in " . E1D_to_str($r);
-		push(@ranges, $r);
-	}
-	
-	#say join("\n", map { E1D_to_str($_) } @ranges);
-	my $b_overlaps = 1;
-	OUTER: while ($b_overlaps) {
-		$b_overlaps = 0;
-		for my $i (0..$#ranges-1) {
-			for my $j (1..$#ranges) {
-				if (E1D_overlaps($ranges[$i], $ranges[$j])) {
-					my $union = E1D_union($ranges[$i], $ranges[$j]);
-					#say E1D_to_str($ranges[$i]) . ' + ' . E1D_to_str($ranges[$j]) . " -> " . E1D_to_str($union);
-					$ranges[$i] = $union;
-					splice(@ranges, $j, 1);
-					$b_overlaps = 1;
-					next OUTER;
-				}
-			}
-		}
-	}
-	#say join("\n", map { E1D_to_str($_) } @ranges);
+	my @ranges = get_impossible_ranges($search_row);
 	
 	my $sum = 0;
 	for my $r (@ranges) {
@@ -89,7 +55,85 @@ sub solve_part_one {
 }
 
 sub solve_part_two {
-	my @input = @_;
+	my $min = 0;
+	my $max = 20;
+	if (scalar(keys %sensors) > 15) {
+		$max = 4000000;
+	}
+	
+	my $possible = [];
+	my $check = E1D_create($min, $max);
+	for my $y ($min..$max) {
+		my @ranges = get_impossible_ranges($y);
+		my $b_ok = 0;
+		for my $r (@ranges) {
+			if (E1D_contains($r, $check)) {
+				$b_ok = 1;
+				last;
+			}
+		}
+		if (!$b_ok) {
+			say "No range completely overlaps " . E1D_to_str($check) . " at y = $y";
+			# Should be two ranges, with a gap of 1 between
+			#say join("\n", map { E1D_to_str($_) } @ranges);
+			if ($ranges[1][0] < $ranges[0][0]) {
+				@ranges = reverse @ranges;
+			}
+			my $x = $ranges[0][1]+1;
+			$possible = [$x, $y];
+			say "possible is " . C2D_to_str($possible);
+			last;
+		}
+		say $y if $y % 250000 == 0;
+	}
+	
+	my $tf = $possible->[0] * 4000000 + $possible->[1];
+	say "Part Two: the tuning frequency is $tf.";
+}
+
+sub get_impossible_ranges {
+	my $search_row = shift;
+	my $DEBUG = 0;
+	
+	# Identify sensors that are within their md of the search row
+	my @close_sensors = ();
+	for my $key (keys %sensors) {
+		my $sensor = $sensors{$key};
+		my $dist_to_search = abs($sensor->{'pos'}[1] - $search_row);
+		push(@close_sensors, $sensor) if $dist_to_search <= $sensor->{'md'};
+	}
+	
+	my @ranges = ();
+	for my $sensor (@close_sensors) {
+		my $dist_to_search = abs($sensor->{'pos'}[1] - $search_row);
+		my $xmin = $sensor->{'pos'}[0] - $sensor->{'md'} + $dist_to_search;
+		my $xmax = $sensor->{'pos'}[0] + $sensor->{'md'} - $dist_to_search;
+		my $r = E1D_create($xmin, $xmax);
+		say "sensor at " . C2D_to_str($sensor->{'pos'}) . " with MD of $sensor->{'md'} means no beacons in " . E1D_to_str($r) if $DEBUG;
+		push(@ranges, $r);
+	}
+	
+	say join("\n", map { E1D_to_str($_) } @ranges) if $DEBUG;
+	my $b_overlaps = 1;
+	OUTER: while ($b_overlaps) {
+		$b_overlaps = 0;
+		for my $i (0..$#ranges-1) {
+			for my $j ($i+1..$#ranges) {
+				if (E1D_overlaps($ranges[$i], $ranges[$j])) {
+					my $union = E1D_union($ranges[$i], $ranges[$j]);
+					say E1D_to_str($ranges[$i]) . ' + ' . E1D_to_str($ranges[$j]) . " -> " . E1D_to_str($union) if $DEBUG;
+					$ranges[$i] = $union;
+					splice(@ranges, $j, 1);
+					$b_overlaps = 1;
+					say join("\n", map { E1D_to_str($_) } @ranges) if $DEBUG;
+					next OUTER;
+				}
+			}
+		}
+	}
+	say "final" if $DEBUG;
+	say join("\n", map { E1D_to_str($_) } @ranges) if $DEBUG;
+	return @ranges;
 }
 
 sub parse_input {
