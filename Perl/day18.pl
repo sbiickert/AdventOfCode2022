9ede493a867758053cb3fe64f0923a7400bf22f2
Simon Biickert <simon@biickert.ca>
2022-12-18 09:02:10 -0700

    Day 18 part 2 complete in Perl

--- Perl/day18.pl
+++ Perl/day18.pl
@@ -25,24 +25,28 @@ say "Advent of Code 2022, Day 18: Boiling Boulders";
 
 my @coords = parse_coords(@input);
 
-solve_part_one(@coords);
-#solve_part_two(@input);
+our $LAVA = 	'#';
+our $AIR = 		'.';
+our $TRAPPED = 	'*';
+our $FREE = 	'~';
+
+our $grid = G3D_create($AIR, 'rook');
+
+for my $coord (@coords) {
+	G3D_set($grid, $coord, $LAVA);
+}
+
+solve_part_one();
+solve_part_two();
 
 exit( 0 );
 
 sub solve_part_one {
-	my @coords = @_;
-	my $grid = G3D_create('.', 'rook');
-	
-	for my $coord (@coords) {
-		G3D_set($grid, $coord, '#');
-	}
-	
 	my $surface_area = 0;
 	for my $coord (@coords) {
 		my @neighbors = G3D_neighbors($grid, $coord);
 		for my $n (@neighbors) {
-			$surface_area++ if (G3D_get($grid, $n) ne '#');
+			$surface_area++ if (G3D_get($grid, $n) ne $LAVA);
 		}
 	}
 	
@@ -50,7 +54,74 @@ sub solve_part_one {
 }
 
 sub solve_part_two {
-	my @input = @_;
+	my $surface_area = 0;
+	for my $coord (@coords) {
+		my @neighbors = G3D_neighbors($grid, $coord);
+		for my $n (@neighbors) {
+			my $n_val = G3D_get($grid, $n);
+			if ($n_val eq $AIR) {
+				$n_val = check_if_trapped($n);
+			}
+			$surface_area++ if ($n_val eq $FREE);
+		}
+	}
+	
+	say "Part Two: surface area is $surface_area.";
+}
+
+sub check_if_trapped {
+	my $start = shift;
+	my $ext = G3D_extent($grid);
+	my @to_visit = ($start);
+	my %visited = ();
+	my $pos = $start;
+	
+	while (scalar(@to_visit) > 0) {
+		my $pos = pop(@to_visit);
+		#say "Checking " . C3D_to_str($pos);
+		
+		# Outside the extent of the droplet
+		my $is_free = !E3D_contains($ext, $pos);
+		if ($is_free) {
+			set_values($FREE, values %visited);
+			return $FREE;
+		}
+		
+		my $val = G3D_get($grid, $pos);
+		
+		if ($val eq $LAVA) {
+			next;
+		}
+		
+		$visited{C3D_to_str($pos)} = $pos;
+		
+		# If it's free, then everything we've visited is free
+		if ($val eq $FREE) {
+			set_values($FREE, values %visited);
+			return $FREE;
+		}
+		# If it's trapped, then everything we've visited is trapped
+		if ($val eq $TRAPPED) {
+			set_values($TRAPPED, values %visited);
+			return $TRAPPED;
+		}
+		
+		for my $n (G3D_neighbors($grid, $pos)) {
+			if (!exists($visited{C3D_to_str($n)})) {
+				push(@to_visit, $n);
+			}
+		}
+	}
+
+	set_values($TRAPPED, values %visited);
+	return $TRAPPED;
+}
+
+sub set_values {
+	my ($value, @coords) = @_;
+	for my $c (@coords) {
+		G3D_set($grid, $c, $value);
+	}
 }
 
 sub parse_coords {
