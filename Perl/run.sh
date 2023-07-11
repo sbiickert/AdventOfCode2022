26cc857097b920fa828f827d0e8e649a74d6e0ac
Simon Biickert <simon@biickert.ca>
2023-07-11 09:21:47 -0600

    Create run.sh

---
+++ Perl/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+perl day01.pl
+perl day02.pl
+perl day03.pl
+perl day04.pl
+perl day05.pl
+perl day06.pl
+perl day07.pl
+perl day08.pl
+perl day09.pl
+perl day10.pl
+perl day11.pl
+perl day12.pl
+perl day13.pl
+perl day14.pl
+perl day17.pl
+perl day18.pl
+perl day20.pl
+perl day21.pl
+perl day22.pl
+perl day23.pl
+perl day25.pl
+
+# perl day15.pl
+# perl day16.pl
+# perl day19.pl
+# perl day24.pl
